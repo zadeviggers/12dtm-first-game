fcc2c6d7fda16ab716d95fa7dd3e9416b8b2181c
Zade Viggers <zade.viggers@gmail.com>
2022-03-29 14:20:33 +1300

    Lava particles

---
+++ Assets/Scripts/MakeLavaRise.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeLavaRise : MonoBehaviour
+{
+    public ParticleSystem particles;
+
+    private float lavaRiseWaitTime = 0.00001f;
+    private float lavaRiseRate = 0.01f;
+
+    void Start()
+    {
+        StartCoroutine(MakeLavalRiseRoutine());
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator MakeLavalRiseRoutine() {
+        yield return new WaitForSeconds(lavaRiseWaitTime);
+        // Increase Y scale
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y + lavaRiseRate, gameObject.transform.localScale.z);
+
+        // Move particles up (need to halve lava rise rate because scaling the lava goes up and down so it seems like it's going up at half speed).
+        particles.gameObject.transform.position += new Vector3(0, lavaRiseRate / 2, 0);
+
+        // Loop
+        StartCoroutine(MakeLavalRiseRoutine());
+    }
+}
