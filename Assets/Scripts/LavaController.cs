190e9dd916f431d11a26e549b87921d46456d8ad
Zade Viggers <zade.viggers@gmail.com>
2022-04-01 15:15:41 +1300

    Add level

--- Assets/Scripts/LavaController.cs
+++ Assets/Scripts/LavaController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundController : MonoBehaviour
+public class LavaController : MonoBehaviour
 {
     public ParticleSystem particlesPrefab;
     private ParticleSystem particles;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
 
     private float walkSpeed = 20.1f;
-    private float jumpSpeed = 1100.1f;
+    private float jumpSpeed = 800.1f;
 
-    private bool canJump = true;
+    private bool isOnGround = false;
+    private bool canJump = false;
 
 
     // Start is called before the first frame update
@@ -24,13 +25,20 @@ public class PlayerController : MonoBehaviour
         // Moving left and right
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        rb.AddForce(Vector2.right * horizontalInput * walkSpeed);
+        // Slow player movement in the air
+        float airSlowDown = 1.0f;
+        if (!isOnGround)
+        {
+            airSlowDown = 0.1f;
+        }
+
+        rb.AddForce(Vector2.right * horizontalInput * walkSpeed * airSlowDown);
 
         // Jumping
         float jumpingInput = Input.GetAxis("Vertical");
         
         // If the input is upwards and the player hasn't jumped...
-        if (jumpingInput > 0 && canJump)
+        if (jumpingInput > 0 && isOnGround && canJump)
         {
             rb.AddForce(Vector2.up * jumpSpeed);
             canJump = false;
@@ -42,6 +50,25 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Deadly"))
         {
             KillPlayer();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            // Allow player to jump when they stand on ground
+            isOnGround = false;
+            canJump = false;
         }
     }
 
