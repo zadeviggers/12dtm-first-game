4508a3f91babafc0f3232745a979b462351a5cc7
Zade Viggers <zade.viggers@gmail.com>
2022-07-26 13:32:40 +1200

    Changes

--- Assets/Scripts/LavaController.cs
+++ Assets/Scripts/LavaController.cs
@@ -18,6 +18,13 @@ public class LavaController : MonoBehaviour
         // Instantiate lava particles
         particles = Instantiate(particlesPrefab);
 
+        // Make sure lava particles are in the same position as the top of the lava
+        particles.transform.position = transform.position - new Vector3(0, transform.localScale.y / 2, 0);
+
+        // Make the particles emitter shape the same width as the lava
+        var shape = particles.shape;
+        shape.scale = new Vector3(transform.localScale.x, shape.scale.y, shape.scale.z);
+
         // Start lava rising
         StartCoroutine(MakeLavalRiseRoutine());
     }
@@ -25,14 +32,14 @@ public class LavaController : MonoBehaviour
     // Looping coroutine that makes the lava rise
     IEnumerator MakeLavalRiseRoutine()
     {
-        // Wait a little before increasing lava rise
-        yield return new WaitForSeconds(lavaRiseWaitTime);
-
-        // Increase Y scale
-        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y + lavaRiseRate, gameObject.transform.localScale.z);
+        // Increase Y scale of lava
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + lavaRiseRate, transform.localScale.z);
 
         // Move particles up (need to halve lava rise rate because scaling the lava goes up and down so it seems like it's going up at half speed).
-        particles.gameObject.transform.position += new Vector3(0, lavaRiseRate / 2, 0);
+        particles.transform.position += new Vector3(0, lavaRiseRate / 2, 0);
+
+        // Wait a little before increasing lava rise
+        yield return new WaitForSeconds(lavaRiseWaitTime);
 
         // Loop
         StartCoroutine(MakeLavalRiseRoutine());
